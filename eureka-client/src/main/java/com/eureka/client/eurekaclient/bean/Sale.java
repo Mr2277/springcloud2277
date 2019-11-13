46a1a6f0f84d3b539f7bfe76949a9380fb93f6c3
sun <837544713@qq.com>
2019-11-13 17:08:02 +0800

    add ribbon 负载均衡

---
+++ eureka-client/src/main/java/com/eureka/client/eurekaclient/bean/Sale.java
@@ -0,0 +1,35 @@
+package com.eureka.client.eurekaclient.bean;
+
+import java.sql.Date;
+
+public class Sale {
+   private Date YMD;
+   private String  REGION;
+   private String FORMATS;
+   private String STORES;
+   private String STORENAME;
+   private String  BILL;
+   private String VIPID;
+   private String  VIP_AGE;
+   private String VIP_SEX;
+   private String SHOPID;
+   private String SHOPNAME;
+   private String  DEPTID;
+   private String DEPTNAME;
+   private String  CLASSID3;
+   private String NAME_CLASS3;
+   private String CLASSID4;
+   private String NAME_CLASS4;
+   private String  CLASSID5;
+   private String NAME_CLASS5;
+   private String STANDARDS;
+   private String UNIT_SALE;
+   private String TIME_DEF;
+   private String PRICE_COM ;
+   private String PRICE_MUST;
+   private String PRICE_SALECHG;
+   private String NUM_SALE;
+   private String VALUE_SALE;
+   private String VALUE_SALE_AFTERDISC;
+   private String ISRTN;
+}
